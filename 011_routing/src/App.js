e40f0c8f63b72f78e824831a8c86af0966928793
C9010554 <ibrahim.kafkasli@arcelik.com>
2021-10-19 15:30:03 +0300

    added routing topic

--- 011_routing/src/App.js
+++ 011_routing/src/App.js
@@ -1,4 +1,10 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
 
 import "./App.css";
 import About from "./components/About";
@@ -38,14 +44,20 @@ function App() {
           <Route path="/">
             <Home />
           </Route>
-        */}
-
+         */}
+          {/*
+          NavLink ile Link arasındaki fark, navlink e style verebiliyoruz, mesela aktif olduğunda rengini değiştirme gibi, aralarındaki fark budur
+          Bir style verdiğimizde, tıpkı route da olduğu gibi ana dizindeki component e exact property si verilmelidir.
+          ayrıca activeClassName de kullanılabilir, bu sayede aktif olanın rengi değişir
+           */}
           {/* Switch calisma mantigi, kok dizin en altta olmalidir, cunku uste alirsak tum componentler ile / ifadesi eslesir ve Home sayfasinda takili kaliriz
               ustte olmasini istiyorsan eger exact property sini eklemen gerekir <Route path="/" exact component={Home}></Route>
           */}
           <Route path="/about" component={About}></Route>
           <Route path="/users" component={Users}></Route>
           <Route path="/user/:id" component={User}></Route>
+          <Route path="*" component={Error404}></Route>{" "}
+          {/* Error page yapmak için kullandık, eşleşme olmayan durumda buna girer */}
           <Route path="/" component={Home}></Route>
         </Switch>
       </div>
---
+++ 011_routing/src/components/Error404.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Error404() {
+  return (
+    <div>
+      <h2>This page was not found!</h2>
+    </div>
+  );
+}
